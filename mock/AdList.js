2491565831a5391d261947ae37c49a68d3e547e2
Administrator <821718601@qq.com>
2020-02-20 22:13:37 +0800

    init

--- mock/AdList.js
+++ mock/AdList.js
@@ -1,8 +1,8 @@
 {
   "code": 0,
   "data": [
-    { "url":"/img/index-swipe-lazy.jpg","src":"/DownloadApp"},
-    { "url":"https://t1.picb.cc/uploads/2020/02/20/k7QoZv.jpg","src":"/DownloadApp"},
+    { "url":"/images/index-swipe-lazy.jpg","src":"/DownloadApp"},
+    { "url":"/images/haosheng-01.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7aweg.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7ay8i.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7atE8.jpg","src":"/DownloadApp"},
