06a2254eeeec9c2096cafffdacd5c4f502acb454
Administrator <821718601@qq.com>
2020-02-21 17:56:08 +0800

    init

--- mock/AdList.js
+++ mock/AdList.js
@@ -3,6 +3,7 @@
   "data": [
     { "url":"/images/index-swipe-lazy.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/21/k7LYLi.jpg","src":"https://u.jd.com/LzPuf2"},
+    { "url":"https://t1.picb.cc/uploads/2020/02/21/k7UeFN.jpg","src":"https://u.jd.com/1QfxyH"},
     { "url":"/images/haosheng-01.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7aweg.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7ay8i.jpg","src":"/DownloadApp"},
