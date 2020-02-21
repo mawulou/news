0f449f7371bc73f2e679d305eff443436b236639
Administrator <821718601@qq.com>
2020-02-21 17:52:45 +0800

    init

--- mock/AdList.js
+++ mock/AdList.js
@@ -2,6 +2,7 @@
   "code": 0,
   "data": [
     { "url":"/images/index-swipe-lazy.jpg","src":"/DownloadApp"},
+    { "url":"https://t1.picb.cc/uploads/2020/02/21/k7LYLi.jpg","src":"https://u.jd.com/LzPuf2"},
     { "url":"/images/haosheng-01.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7aweg.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7ay8i.jpg","src":"/DownloadApp"},
