edc36d94b0171265a7e7e29b95d2b347afdbf4bc
Administrator <821718601@qq.com>
2020-02-21 18:05:13 +0800

    init

--- mock/AdList.js
+++ mock/AdList.js
@@ -2,8 +2,8 @@
   "code": 0,
   "data": [
     { "url":"/images/index-swipe-lazy.jpg","src":"/DownloadApp"},
-    { "url":"https://t1.picb.cc/uploads/2020/02/21/k7LYLi.jpg","src":"https://u.jd.com/LzPuf2"},
-    { "url":"https://t1.picb.cc/uploads/2020/02/21/k7UeFN.jpg","src":"https://u.jd.com/1QfxyH"},
+    { "url":"https://t1.picb.cc/uploads/2020/02/21/k7LYLi.jpg","src":"https://u.jd.com/LzPuf2","title":"京东远程办公直降，主力中国"},
+    { "url":"https://t1.picb.cc/uploads/2020/02/21/k7UeFN.jpg","src":"https://u.jd.com/1QfxyH","title":"你家装修花多少钱?获取装修报价"},
     { "url":"/images/haosheng-01.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7aweg.jpg","src":"/DownloadApp"},
     { "url":"https://t1.picb.cc/uploads/2020/02/20/k7ay8i.jpg","src":"/DownloadApp"},
