3f88f2fbbe3702e0e243d2855d96dea84ae81951
Administrator <821718601@qq.com>
2020-02-23 17:18:11 +0800

    init

---
+++ mock/ArticleList_BianMin.js
@@ -0,0 +1,8 @@
+{
+  "code": 0,
+  "data": [
+    { "title": "物业","href":"/html/article/wuye.html"},
+    { "title": "新奥天然气","href":"/html/article/xinao.html"}
+
+  ]
+}
---
+++ mock/ArticleList_FuLi.js
@@ -0,0 +1,14 @@
+{
+  "code": 0,
+  "data": [
+    { "title": "每日必抢","href":"http://www.tzbridge.cn/mall/topic?type=9&isc=1&goodSource=1&sign=1qCb3&title=%E4%BB%8A%E6%97%A5%E5%BF%85%E6%8A%A2"},
+    { "title": "充电宝到手只要24.9！速抢！！【只换不修可登机~】",
+         "href":"http://www.tzbridge.cn/mall/detail?itemId=610992304959&activityId=f1463e602baf4567a2b3ad44e04c4337&goodSource=1&sign=1qCb3"
+     },
+    {
+        "title":"大岭山镇2019年市重大项目建设工作",
+        "href":"http://www.dg.gov.cn/zwgk/zfxxgkml/dlsz/qt/gzdt/content/post_2826887.html"
+    }
+
+  ]
+}
